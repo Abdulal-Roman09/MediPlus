20bbde9e8c2987da945b71ff394a6e935980389e
Abdul Al Roman <mdroman45678910@gmail.com>
2025-12-11 02:47:08 +0600

    feat: add SUPER_ADMIN role to UserRole enum and update routes for role-based access control

--- src/app/modules/Admin/admin.routes.ts
+++ src/app/modules/Admin/admin.routes.ts
@@ -2,18 +2,36 @@ import express from 'express';
 import { AdminController } from './admin.controller';
 import validateRequest from '../../middleWares/validateRequest';
 import { AdminValidationSchema } from './admin.validation';
+import auth from '../../middleWares/auth';
+import { UserRole } from '@prisma/client';
 
 
 const router = express.Router();
 
-router.get("/", AdminController.getAllAdmin);
+router.get(
+    "/",
+    auth(UserRole.ADMIN, UserRole.SUPER_ADMIN),
+    AdminController.getAllAdmin);
 
-router.get("/:id", AdminController.getSingleAdmin);
+router.get(
+    "/:id",
+    auth(UserRole.ADMIN, UserRole.SUPER_ADMIN),
+    AdminController.getSingleAdmin);
 
-router.patch("/:id", validateRequest(AdminValidationSchema.updateAdminZodSchema), AdminController.updateAdmin);
+router.patch(
+    "/:id",
+    auth(UserRole.ADMIN, UserRole.SUPER_ADMIN),
+    validateRequest(AdminValidationSchema.updateAdminZodSchema),
+    AdminController.updateAdmin);
 
-router.delete("/:id", AdminController.deleteAdmin);
+router.delete(
+    "/:id",
+    auth(UserRole.ADMIN, UserRole.SUPER_ADMIN),
+    AdminController.deleteAdmin);
 
-router.delete("/soft/:id", AdminController.softDeleteAdmin);
+router.delete(
+    "/soft/:id",
+    auth(UserRole.ADMIN, UserRole.SUPER_ADMIN),
+    AdminController.softDeleteAdmin);
 
 export const AdminRoutes = router;
--- src/app/modules/User/user.routes.ts
+++ src/app/modules/User/user.routes.ts
@@ -1,9 +1,10 @@
 import express, { Request, Response } from 'express';
 import { UserController } from './user.controller';
 import auth from '../../middleWares/auth';
+import { UserRole } from '@prisma/client';
 
 const router = express.Router();
 
-router.post("/", auth("ADMIN"), UserController.createAdmin);
+router.post("/", auth(UserRole.ADMIN), UserController.createAdmin);
 
 export const UserRoutes = router;
