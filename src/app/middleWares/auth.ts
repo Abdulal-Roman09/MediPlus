4a545f16fd0dcd2184ee2bd198aa4319f6193ea8
Abdul Al Roman <mdroman45678910@gmail.com>
2025-12-11 02:27:18 +0600

    feat: implement custom AppError class and update auth middleware to use it for error handling

--- src/app/middleWares/auth.ts
+++ src/app/middleWares/auth.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from "express"
 import { verifyToken } from "../../halpers/jwtHelper"
 import config from "../../config"
+import AppError from "../errors/AppError"
+import httpStatus from "http-status"
 
 
 const auth = (...roles: string[]) => {
@@ -8,11 +10,11 @@ const auth = (...roles: string[]) => {
         try {
             const token = req.headers.authorization
             if (!token) {
-                throw new Error("you are nto authorizatied")
+                throw new AppError(httpStatus.UNAUTHORIZED, "you are nto authorizatied")
             }
             const verifyUser = verifyToken(token, config.jwt.secret)
             if (roles.length && !roles.includes(verifyUser.role)) {
-                throw new Error("you are nto authorizatied")
+                throw new AppError(httpStatus.UNAUTHORIZED, "you are nto authorizatied")
             }
             next()
         } catch (err) {
