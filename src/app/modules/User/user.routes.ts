f208dab7a3a4f7e9117c9a36f593724277bf9645
Abdul Al Roman <mdroman45678910@gmail.com>
2025-12-12 00:03:33 +0600

    feat(upload): add file uploader functionality using multer

--- src/app/modules/User/user.routes.ts
+++ src/app/modules/User/user.routes.ts
@@ -2,12 +2,16 @@ import express, { Request, Response } from 'express';
 import { UserController } from './user.controller';
 import auth from '../../middleWares/auth';
 import { UserRole } from '@prisma/client';
+import fileUploader from '../../../halpers/fileUploader';
+
 
 const router = express.Router();
 
+
 router.post(
     "/",
-    auth(UserRole.ADMIN),
+    auth(UserRole.ADMIN, UserRole.SUPER_ADMIN),
+    fileUploader.upload.single('file'),
     UserController.createAdmin);
 
 export const UserRoutes = router;
