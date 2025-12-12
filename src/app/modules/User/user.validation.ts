7519f9365638345d38c4cd3a0e9bf3cff78ec222
Abdul Al Roman <mdroman45678910@gmail.com>
2025-12-12 20:14:26 +0600

    feat:patient validation is created

--- src/app/modules/User/user.validation.ts
+++ src/app/modules/User/user.validation.ts
@@ -28,7 +28,18 @@ const createDoctor = z.object({
   }),
 });
 
+const createPatient = z.object({
+  password: z.string().nonempty("Password is required"),
+  patient: z.object({
+    name: z.string().nonempty("Name is required!"),
+    email: z.string().nonempty("Email is required!"),
+    contactNumber: z.string().nonempty("Contact Number is required!"),
+    address: z.string().optional(),
+  }),
+});
+
 export const UserValidationSchema = {
   createAdmin,
-  createDoctor
+  createDoctor,
+  createPatient
 };
